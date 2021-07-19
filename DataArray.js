360b69e5ea9449e69fb2d6d456ad601f5f95985c
Glitch (glitch-blank-node) <none>
2021-07-19 12:55:15 +0000

    💶🏙 Checkpoint ./DataArray.js:6372283/572

--- DataArray.js
+++ DataArray.js
@@ -1,7 +1,21 @@
 class DataArray{
-  constructor(){
-
-    
+  
+  /**
+  * Конструктор 
+  */
+  
+  constructor(data,options = {limit:-1}){
     this.data = [[],[]]
+  }
+  
+  /**
+  * Get
+  * Возвращает значение под ключом key
+  * @param {any} key ключ элемента
+  * @return {any} значение, закрепленное за key
+  */
+  
+  Get(key){
+    return this.data[1][this.data[0].indexOf(key)]||null
   }
 }
