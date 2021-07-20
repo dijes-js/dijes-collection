708c10f75bb90b10d7b2f74e89515ca10f7c0d3b
Glitch (glitch-blank-node) <none>
2021-07-20 09:00:26 +0000

    🏥🖼 Checkpoint ./Collection.js:6372283/611:9219272/1036

--- Collection.js
+++ Collection.js
@@ -1,5 +1,18 @@
-class Collection {
-  
+module.exports = class Collection {
+  constructor({ limit = -1 } = {}, data = [[], []]) {
+    this.limit = typeof limit == "number" ? limit : -1;
+    this.data =
+      data instanceof Array &&
+      data[0] &&
+      data[1] &&
+      data[0] instanceof Array &&
+      data[1] instanceof Array
+        ? data
+        : [[], []];
+  }
 }
-
-module.exports = Collection
+//
+//интегрируй код, он не совсем верный для выбранной структуры данных
+//лучше возьми конструктор из dataArray различий не особо много
+//идентифицируй массив, в котором будут храниться данные
+//метод constructor() вызывается при создании экземпляра класса (new Class())
